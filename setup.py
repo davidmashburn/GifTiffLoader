656f310a8ff4c577645fa33a96c6038f264856d4
David N. Mashburn <david.n.mashburn@gmail.com>
2012-08-21 14:37:54 -0500

    Fixed errors in setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from distutils.core import setup
 
 setup(
-    name='GifTiffloader',
+    name='GifTiffLoader',
     version='0.1',
     author='David N. Mashburn',
     author_email='david.n.mashburn@gmail.com',
-    packages=['GifTiffloader'],
+    packages=['GifTiffLoader'],
     scripts=[],
-    url='http://pypi.python.org/pypi/GifTiffloader/',
+    url='http://pypi.python.org/pypi/GifTiffLoader/',
     license='LICENSE.txt',
     description='',
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
     install_requires=[
          "numpy >= 0.9",
          "PIL >= 1.1.5",
